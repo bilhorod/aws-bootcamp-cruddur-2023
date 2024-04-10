01c3401837c8d4903c77a76c99865488e78af934
Artem Tarkhanyan <artem@arimus.com>
2024-04-10 07:57:34 +0000

    cloudwatch

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -29,6 +29,20 @@ from opentelemetry.sdk.trace.export import ConsoleSpanExporter, SimpleSpanProces
 from aws_xray_sdk.core import xray_recorder 
 from aws_xray_sdk.ext.flask.middleware import XRayMiddleware 
 
+#cloudwatch logs
+import watchtower
+import logging
+from time import strftime
+
+# Configuring Logger to Use Cloudwatch
+LOGGER = logging.getLogger(__name__)
+LOGGER.setLevel(logging.DEBUG)
+console_handler = logging.StreamHandler()
+cw_handler = watchtower.CloudWatchLogHandler(log_group='cruddur')
+LOGGER.addHandler(console_handler)
+LOGGER.addHandler(cw _handler)
+LOGGER.info("some message")
+
 #Honeycomb
 provider = TracerProvider()
 processor = BatchSpanProcessor(OTLPSpanExporter())
@@ -66,6 +80,14 @@ cors = CORS(
   allow_headers="content-type,if-modified-since",
   methods="OPTIONS,GET,HEAD,POST"
 )
+
+
+@app.after_request
+def after_request(response):
+  timestamp = strftime('[%Y-%b-%d %H:%M]')
+  LOGGER.error('%s %s %s %s %s %s', timestamp, request.remote_addr, request.method, request.scheme, request.full_path, response.status)
+  return response
+
 
 @app.route("/api/message_groups", methods=['GET'])
 def data_message_groups():
