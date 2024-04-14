50590f0d6db6443c6d8b89085a4252a0908e8057
Artem Tarkhanyan <artem@arimus.com>
2024-04-14 01:43:42 +0000

    commenting out xray and cloud watch

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -5,6 +5,7 @@ import os
 
 
 from services.home_activities import *
+from services.user_activities import *
 from services.notifications_activities import *
 from services.create_activity import *
 from services.create_reply import *
@@ -13,7 +14,7 @@ from services.message_groups import *
 from services.messages import *
 from services.create_message import *
 from services.show_activity import *
-from services.user_activities import *
+
 
 
 #honeycomb
@@ -26,8 +27,8 @@ from opentelemetry.sdk.trace.export import BatchSpanProcessor
 from opentelemetry.sdk.trace.export import ConsoleSpanExporter, SimpleSpanProcessor
 
 #xray
-from aws_xray_sdk.core import xray_recorder 
-from aws_xray_sdk.ext.flask.middleware import XRayMiddleware 
+#from aws_xray_sdk.core import xray_recorder 
+#from aws_xray_sdk.ext.flask.middleware import XRayMiddleware 
 
 #cloudwatch logs
 import watchtower
@@ -35,13 +36,14 @@ import logging
 from time import strftime
 
 # Configuring Logger to Use Cloudwatch
-LOGGER = logging.getLogger(__name__)
-LOGGER.setLevel(logging.DEBUG)
-console_handler = logging.StreamHandler()
-cw_handler = watchtower.CloudWatchLogHandler(log_group='cruddur')
-LOGGER.addHandler(console_handler)
-LOGGER.addHandler(cw _handler)
-LOGGER.info("some message")
+#LOGGER = logging.getLogger(__name__)
+#LOGGER.setLevel(logging.DEBUG)
+#console_handler = logging.StreamHandler()
+#cw_handler = watchtower.CloudWatchLogHandler(log_group='cruddur')
+#LOGGER.addHandler(console_handler)
+#LOGGER.addHandler(cw_handler)
+#LOGGER.info("TestLog")
+
 
 #Honeycomb
 provider = TracerProvider()
@@ -61,8 +63,9 @@ trace.set_tracer_provider(provider)
 tracer = trace.get_tracer(__name__)
 
 app = Flask(__name__)
+
 #xray
-XRayMiddleware(app, xray_recorder)
+#XRayMiddleware(app, xray_recorder)
 
 #Honeycomb
 #Initialize automatic instrumentat ion with Flask
@@ -82,11 +85,11 @@ cors = CORS(
 )
 
 
-@app.after_request
-def after_request(response):
-  timestamp = strftime('[%Y-%b-%d %H:%M]')
-  LOGGER.error('%s %s %s %s %s %s', timestamp, request.remote_addr, request.method, request.scheme, request.full_path, response.status)
-  return response
+#@app.after_request
+#def after_request(response):
+#  timestamp = strftime('[%Y-%b-%d %H:%M]')
+#  LOGGER.error('%s %s %s %s %s %s', timestamp, request.remote_addr, request.method, request.scheme, request.full_path, response.status)
+#  return response
 
 
 @app.route("/api/message_groups", methods=['GET'])
--- backend-flask/services/user_activities.py
+++ backend-flask/services/user_activities.py
@@ -3,7 +3,7 @@ from aws_xray_sdk.core import xray_recorder
 
 class UserActivities:
   def run(user_handle):
-    segment = xray_recorder.begin_segment('user_activities')
+    #segment = xray_recorder.begin_segment('user_activities')
     model = {
       'errors': None,
       'data': None
@@ -12,23 +12,25 @@ class UserActivities:
     now = datetime.now(timezone.utc).astimezone()
 
     if user_handle == None or len(user_handle) < 1:
-      model['errors'] = ['blank_user_handle']
+        model['errors'] = ['blank_user_handle']
     else:
-      now = datetime.now()
-      results = [{
-        'uuid': '248959df-3079-4947-b847-9e0892d1bab4',
-        'handle':  'Andrew Brown',
-        'message': 'Cloud is very fun!',
-        'created_at': (now - timedelta(days=1)).isoformat(),
-        'expires_at': (now + timedelta(days=31)).isoformat()
-      }]
-      model['data'] = results
-    subsegment = xray_recorder.begin_subsegment('mock-data')
-        #xray
-    dict= {
-      "now" : now.isoformat(),
-      "results-size" : len(model['data'])
-    }
-    subsegment.put_metadata('key', dict, "namespace")
+        now = datetime.now()
+        results = [{
+            'uuid': '248959df-3079-4947-b847-9e0892d1bab4',
+            'handle': 'Andrew Brown',
+            'message': 'Cloud is very fun!',
+            'created_at': (now - timedelta(days=1)).isoformat(),
+            'expires_at': (now + timedelta(days=31)).isoformat()
+        }]
+        model['data'] = results
+
+
+#    subsegment = xray_recorder.begin_subsegment('mock-data')
+#XRAY
+#    dict= {
+#      "now" : now.isoformat(),
+#      "results-size" : len(model['data'])
+#    }
+#    subsegment.put_metadata('key', dict, "namespace")
     return model
     
