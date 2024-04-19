928f25a5e6ecb7ff77213336a7f6fc759dc20626
Artem Tarkhanyan <artem@arimus.com>
2024-04-19 05:39:19 +0000

    integrate cognito

--- frontend-react-js/src/App.js
+++ frontend-react-js/src/App.js
@@ -15,6 +15,24 @@ import {
   RouterProvider
 } from "react-router-dom";
 
+import { Amplify } from 'aws-amplify' ;
+
+Amplify.configure({
+  "AWS_PROJECT_REGION": process.env.REACT_APP_AWS_PROJECT_REGION,
+  "aws_cognito_identity_pool_id": process.env.REACT_APP_AWS_COGNITO_IDENTITY_POOL_ID,
+  "aws_cognito_region": process.env.REACT_APP_AWS_COGNITO_REGION,
+  "aws_user_pools_id": process.env.REACT_APP_AWS_USER_POOLS_ID,
+  "aws_user_pools_web_client_id": process.env.REACT_APP_CLIENT_ID,
+  "oauth": {},
+  Auth: {
+    // We are not using an Identity Pool
+    // identityPoolId: process. env. REACT_APP IDENTITY_ POOL ID, // REQUIRED - Amazon Cogn 
+    region: process.env.REACT_AWS_PROJECT_REGION,      // REQUIRED - Amazon Cognito
+    userPoolId: process.env.REACT_APP_AWS_USER_POOLS_ID, // OPTIONAL - Amazon Co
+    userPoolWebClientId: process.env.REACT_APP_AWS_USER_POOLS_WEB_CLIENT_ID, // OPTION
+  }
+});
+
 const router = createBrowserRouter([
   {
     path: "/",
